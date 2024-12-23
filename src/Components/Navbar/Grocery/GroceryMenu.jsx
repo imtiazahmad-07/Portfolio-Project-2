fe432c1730bd3144c450dacacd5ff2f6ddf2a6a5
imtiazahmad-07 <7807imtiaz@gmail.com>
2024-12-24 00:50:34 +0500

    Some Routes in Grocery Menu

--- src/Components/Navbar/Grocery/GroceryMenu.jsx
+++ src/Components/Navbar/Grocery/GroceryMenu.jsx
@@ -1,9 +1,18 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import { Apple, Armchair, BriefcaseBusiness, Brush, Cookie, Shirt } from 'lucide-react';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import {
+  Apple,
+  Armchair,
+  BriefcaseBusiness,
+  Brush,
+  Cookie,
+  Shirt,
+} from "lucide-react";
+import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
 
 export default function GroceryMenu() {
   const [item, setItem] = React.useState("Grocery");
@@ -13,22 +22,73 @@ export default function GroceryMenu() {
   };
 
   return (
-    <Box sx={{width:"170px"}}>
-      <FormControl sx={{width:"150px"}}>
+    <Box sx={{ width: "170px" }}>
+      <FormControl sx={{ width: "150px" }}>
         <Select
-        sx={{color:'#009F7F'}}
+          sx={{ color: "#009F7F" }}
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           value={item}
           onChange={handleChange}
         >
-          <MenuItem sx={{display:"flex", gap:"10px", color:"#009F7F"}} value="Grocery"> <Apple />  Grocery </MenuItem>
-          <MenuItem sx={{display:"flex", gap:"10px", color:"#009F7F"}} value="Bakery"> <Cookie /> Bakery</MenuItem>
-          <MenuItem sx={{display:"flex", gap:"10px", color:"#009F7F"}} value="Makeup"> <Brush /> Makeup</MenuItem>
-          <MenuItem sx={{display:"flex", gap:"10px", color:"#009F7F"}} value="Bags"> <BriefcaseBusiness/> Bags</MenuItem>
-          <MenuItem sx={{display:"flex", gap:"10px", color:"#009F7F"}} value="Clothing"> <Shirt /> Clothing</MenuItem>
-          <MenuItem sx={{display:"flex", gap:"10px", color:"#009F7F"}} value="Furniture"> <Armchair /> Furniture</MenuItem>
-          <MenuItem sx={{display:"flex", gap:"10px", color:"#009F7F"}} value="DailyNeeds"> <Apple />Daily Needs</MenuItem>
+          <MenuItem
+            sx={{
+              display: "flex",
+              gap: "10px",
+              color: "#009F7F",
+              textDecoration: "none",
+            }}
+            value="Grocery"
+          >
+            <Link to={"/"} style={{ textDecoration: "none" }} >
+              <Typography sx={{ color: "#009F7F"}}>
+                <Apple /> Grocery
+              </Typography>
+            </Link>
+          </MenuItem>
+          <MenuItem sx={{ display: "flex", gap: "10px" }} value="Bakery">
+            <Link to={"bakery"} style={{ textDecoration: "none" }}>
+              <Typography sx={{ color: "#009F7F", textDecoration: "none" }}>
+                <Cookie /> Bakery
+              </Typography>
+            </Link>
+          </MenuItem>
+          <MenuItem
+            sx={{ display: "flex", gap: "10px", color: "#009F7F" }}
+            value="Makeup"
+          >
+            {" "}
+            <Brush /> Makeup
+          </MenuItem>
+          <MenuItem
+            sx={{ display: "flex", gap: "10px", color: "#009F7F" }}
+            value="Bags"
+          >
+            {" "}
+            <BriefcaseBusiness /> Bags
+          </MenuItem>
+          <MenuItem
+            sx={{ display: "flex", gap: "10px", color: "#009F7F" }}
+            value="Clothing"
+          >
+            {" "}
+            <Shirt /> Clothing
+          </MenuItem>
+          <MenuItem
+            sx={{ display: "flex", gap: "10px", color: "#009F7F" }}
+            value="Furniture"
+          >
+            {" "}
+            <Armchair /> Furniture
+          </MenuItem>
+          <MenuItem
+            sx={{ display: "flex", gap: "10px", color: "#009F7F" }}
+            value="DailyNeeds"
+          >
+            {" "}
+            <Apple />
+            Daily Needs
+          </MenuItem>
         </Select>
       </FormControl>
     </Box>

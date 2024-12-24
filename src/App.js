aca30259e328871f6ead00a6d5dbdff4f5c7c9e0
imtiazahmad-07 <7807imtiaz@gmail.com>
2024-12-24 13:46:02 +0500

    Some deletion from components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import Navbar from "./Navbar/Navbar";
-import Groceries from "./Groceries/Groceries";
 
 function App() {
   return (
     <div >
-      <Navbar/>
-      <Groceries/>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import Groceries from "./Components/Groceries/Groceries";
 import SingleProduct from "./Components/Groceries/ProductItems/SingleProduct/SingleProduct.jsx";
 import BakeryProduct from "./Components/Bakery/BakeryProduct/BakeryProduct.jsx";
 import BakeryProductDetail from "./Components/Bakery/BakeryProductDetail/BakeryProductDetail.jsx";
+import { Provider } from "react-redux";
+import { store } from "./store.js";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -34,6 +36,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router} />
+    </Provider>,
+    
   </React.StrictMode>
 );

c33440af8739117a93d0ed4bdcd2ca2a7757951a
imtiazahmad-07 <7807imtiaz@gmail.com>
2024-12-24 21:58:23 +0500

    Removed unnecessary stuffs and renamed Groceries to HomePageHomepage

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./Components/Layout/Layout"; // Import the layout that includes Navbar
-import Groceries from "./Components/Groceries/HomePage.jsx";
-import SingleProduct from "./Components/Groceries/ProductItems/SingleProduct/SingleProduct.jsx";
+import Layout from "./Components/Layout/Layout"
 import BakeryProduct from "./Components/Bakery/BakeryProduct/BakeryProduct.jsx";
 import BakeryProductDetail from "./Components/Bakery/BakeryProductDetail/BakeryProductDetail.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store.js";
 import "react-toastify/dist/ReactToastify.css";
+import HomePage from "./Components/Home/HomePage.jsx";
+import SingleProduct from "./Components/Home/ProductItems/SingleProduct/SingleProduct.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />, // This will render Navbar and Outlet
+    element: <Layout />, 
     errorElement: <h1>Error 404 Not Found</h1>,
     children: [
       {
         path: "",
-        element: <Groceries />, // Main page that will render under the navbar
+        element: <HomePage />, 
       },
       {
         path: "product-detail/:id",

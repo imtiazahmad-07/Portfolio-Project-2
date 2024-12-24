de9d449b2a681b5e6a6cefc594c5074d30382bf4
imtiazahmad-07 <7807imtiaz@gmail.com>
2024-12-24 21:00:15 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./Components/Layout/Layout";  // Import the layout that includes Navbar
-import Groceries from "./Components/Groceries/Groceries";
+import Layout from "./Components/Layout/Layout"; // Import the layout that includes Navbar
+import Groceries from "./Components/Groceries/HomePage.jsx";
 import SingleProduct from "./Components/Groceries/ProductItems/SingleProduct/SingleProduct.jsx";
 import BakeryProduct from "./Components/Bakery/BakeryProduct/BakeryProduct.jsx";
 import BakeryProductDetail from "./Components/Bakery/BakeryProductDetail/BakeryProductDetail.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store.js";
+import "react-toastify/dist/ReactToastify.css";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,  // This will render Navbar and Outlet
-    errorElement:<h1>Error 404 Not Found</h1>,
+    element: <Layout />, // This will render Navbar and Outlet
+    errorElement: <h1>Error 404 Not Found</h1>,
     children: [
       {
         path: "",
-        element: <Groceries />,  // Main page that will render under the navbar
+        element: <Groceries />, // Main page that will render under the navbar
       },
       {
         path: "product-detail/:id",
-        element: <SingleProduct/>,  
+        element: <SingleProduct />,
       },
       {
-        path:"bakery",
-        element: <BakeryProduct/>
+        path: "bakery",
+        element: <BakeryProduct />,
       },
       {
-        path:"bakery/bakery-product-detail/:id",
-        element:<BakeryProductDetail/>
-      }
+        path: "bakery/bakery-product-detail/:id",
+        element: <BakeryProductDetail />,
+      },
     ],
   },
 ]);
@@ -37,8 +38,8 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <RouterProvider router={router} />
-    </Provider>,
-    
+      <RouterProvider router={router} />
+    </Provider>
+    ,
   </React.StrictMode>
 );

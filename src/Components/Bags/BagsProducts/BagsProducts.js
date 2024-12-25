b047814749301461296fa74bbc0025d81e8a8420
imtiazahmad-07 <7807imtiaz@gmail.com>
2024-12-25 22:51:11 +0500

    imported images for details

---
+++ src/Components/Bags/BagsProducts/BagsProducts.js
@@ -0,0 +1,16 @@
+import img1 from './BagsImages/1.webp'
+import img2 from './BagsImages/2.webp';
+import img3 from './BagsImages/3.webp';
+import img4 from './BagsImages/4.webp';
+import img5 from './BagsImages/5.webp';
+import img6 from './BagsImages/6.webp';
+import img7 from './BagsImages/7.webp';
+import img8 from './BagsImages/8.webp';
+import img9 from './BagsImages/9.webp';
+import img10 from './BagsImages/10.webp';
+import img11 from './BagsImages/11.webp';
+import img12 from './BagsImages/12.webp';
+import img13 from './BagsImages/13.webp';
+import img14 from './BagsImages/14.webp';
+import img15 from './BagsImages/15.webp';
+

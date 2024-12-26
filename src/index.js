1eef688136f03c9845ef5b85800672d7e3a25c9d
imtiazahmad-07 <7807imtiaz@gmail.com>
2024-12-26 17:53:35 +0500

    Routed to clothing

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ import Makeup from "./Components/Makeup/Makeup.jsx";
 import UniqueProduct from "./Components/Makeup/UniqueProduct/UniqueProduct.jsx";
 import BagsProduct from "./Components/Bags/BagsProduct/BagsProduct.jsx";
 import BagsProductDetail from "./Components/Bags/BagsProductDetail/BagsProductDetail.jsx";
+import ClothesCards from "./Components/Cllothing/ClothesCard/ClothesCards.jsx";
+import ClothProductDetail from "./Components/Cllothing/ClothProductDetail/ClothProductDetail.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -50,6 +52,14 @@ const router = createBrowserRouter([
       {
         path:"bags/product-detail/:id",
         element:<BagsProductDetail/>
+      },
+      {
+        path:"clothing",
+        element:<ClothesCards/>
+      },
+      {
+        path:"clothing/product-detail/:id",
+        element:<ClothProductDetail/>
       }
     ],
   },
